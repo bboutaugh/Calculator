e6033983f325defd77d55d6e03e1bded4178b1af
bradb <bradb@DESKTOP-76UD862>
2018-07-30 22:07:36 -0500

    commit2 07/30/18

--- src/calculator/MathOperations.java
+++ src/calculator/MathOperations.java
@@ -9,7 +9,49 @@ package calculator;
  *
  * @author bradb
  */
+
+import static java.lang.Math.pow;
+import java.math.*;
+
 public class MathOperations 
 {
+    double number1 = 0;
+    double number2 = 0;
+    double result = 0;
     
+    public double add(double number1,double number2)
+    {
+        result = number1 + number2;
+        return result;
+    }
+    
+    public double subtract(double number1, double number2)
+    {
+        result = number1 - number2;
+        return result;
+    }
+    
+    public double multiply(double number1, double number2)
+    {
+        result = number1 * number2;
+        return result;
+    }
+    
+    public double divide(double number1, double number2)
+    {
+        result = number1/number2;
+        return result;
+    }
+    
+    public double square(double number1)
+    {
+        result = number1 *= number1;
+        return result;
+    }
+    
+    public double squareRoot(double number1)
+    {
+        result = pow(number1,0.5);
+        return result;
+    }
 }
